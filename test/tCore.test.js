d547231471e091f9a94418a81f1fcd3fd746e838
Bruce McLean <mcleanb@photo-nomad.net>
2018-09-18 21:50:15 -0400

    Am able to navigate the app now

---
+++ test/tCore.test.js
@@ -0,0 +1,46 @@
+/* eslint-disable quotes,no-console */
+const tCoreConnect = require('./tCoreConnect');
+
+describe('Sample Test', () => {
+  let app;
+
+  beforeEach(async () => {
+    app = await tCoreConnect.startApp();
+  });
+
+  afterEach(async() => {
+    await tCoreConnect.stopApp(app);
+  });
+
+  it('opens a window', async() => {
+    console.log("starting");
+    await app.client.pause(5000).waitUntilWindowLoaded()
+      .getWindowCount()
+      .should.eventually.not.equal(0);
+    await app.client.browserWindow.isVisible().should.eventually.equal(true);
+    await app.client.windowByIndex(1).waitUntilWindowLoaded().click('button*=Get');
+    await app.client.pause(1000).click('button*=Project');
+    await app.client.pause(1000).click('button[tabindex="0"]');
+    await app.client.pause(1000).click('#content > div > div.container-fluid > div:nth-child(2) > div > div > div:nth-child(2) > div:nth-child(2) > div > div > div:nth-child(2) > div > div > div:nth-child(2) > div:nth-child(2) > button');
+    console.log("showing local import");
+    await app.client.pause(5000).getTitle().should.equal('Five');
+  });
+
+  // it('should get a url', async() => {
+  //   await app.client.pause(1000).url(config.url)
+  //     .getTitle()
+  //     .should.eventually.include('DuckDuckGo');
+  // });
+  //
+  // it('should search', async() => {
+  //   const input = 'this is a test';
+  //   await app.client.url(config.url)
+  //     .setValue(SearchPage.searchField, input)
+  //     .getValue(SearchPage.searchField)
+  //     .should.eventually.equal(input)
+  //     .click(SearchPage.searchButton)
+  //     .element(SearchPage.searchResult)
+  //     .should.eventually.exist;
+  // });
+
+});
---
+++ test/tCoreConnect.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+const Application = require('spectron').Application;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const electron = require('electron');
+const path = require('path');
+
+const appPath = path.join(__dirname, '../../translationCore/src/main.js');
+console.log('appPath', appPath);
+
+global.before(() => {
+  chai.should();
+  chai.use(chaiAsPromised);
+});
+
+module.exports = {
+  async startApp() {
+    const app = await new Application({
+      path: electron,
+      args: [appPath]
+    }).start();
+    chaiAsPromised.transferPromiseness = app.transferPromiseness;
+    return app;
+  },
+
+  async stopApp(app) {
+    if (app && app.isRunning()) {
+      await app.stop();
+    }
+  }
+};
