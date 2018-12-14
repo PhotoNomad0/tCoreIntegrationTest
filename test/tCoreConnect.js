81ed89c4ec7d4844a33f9c6daf6317014ed1a4a2
Bruce McLean <mcleanb@photo-nomad.net>
2018-12-14 06:13:50 -0500

    suceeded in getting the app to start up.

--- test/tCoreConnect.js
+++ test/tCoreConnect.js
@@ -6,7 +6,7 @@ const electron = require('electron');
 const path = require('path');
 const dialogAddon = require('spectron-dialog-addon').default;
 
-const appPath = path.join(__dirname, '../../testDir/translationCoreTest/src/main.js'); // launch from source
+const appPath = path.join(__dirname, '../../testDir/translationCoreTest/src/es6-init.js'); // launch from source
 
 /*
   When running compiled app as below, spectron launches the app and can click the get started button that shows the app, but hangs trying to click on "Project Navigation" at top of app screen.  Not sure why that would be.
@@ -22,19 +22,20 @@ global.before(() => {
 
 module.exports = {
   async startApp() {
-    const useElectron = (appPath.indexOf('main.js') >= 0);
+    const useElectron = true; // (appPath.indexOf('main.js') >= 0);
     let app;
     if (useElectron) {
       app = new Application({
         path: electron,
         args: [appPath]
       });
-    } else {
-      // launch app directly
-      app = new Application({
-        path: appPath
-      });
     }
+    // else {
+    //   // launch app directly
+    //   app = new Application({
+    //     path: appPath
+    //   });
+    // }
     dialogAddon.apply(app);
     await app.start();
     chaiAsPromised.transferPromiseness = app.transferPromiseness;
